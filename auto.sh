67c2c0ee570adf3bcd47b92b5c937f7a814f9694
Chen Feng <puck.chen@foxmail.com>
2020-04-05 00:37:54 +0800

    init: fix some init scripts
    Signed-off-by: Chen Feng <puck.chen@foxmail.com>

--- auto.sh
+++ auto.sh
@@ -2,7 +2,9 @@
 yes | sudo apt-get install ctags cscope vim git global clang clang-format
 git config --global user.name "Chen Feng"
 git config --global user.email "puck.chen@foxmail.com"
-git config core.editor vim
+git config --global core.editor vim
+mkdir -p ~/work/github
+
 cp ./vimrc ~/.vim/vimrc
 cp ./linux-format ~/.vim/linux-format
 cp linux-format ~/bin/
@@ -11,7 +13,7 @@ touch ~/.vim/cscope/cscope.files
 mkdir -p ~/bin
 cp ./cs ~/bin
 sudo chmod a+x ~/bin/cs
-sudo echo 'export PATH="$PATH:$HOME/bin"' >> ~/.bashrc
+sudo echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
 sudo echo 'set completion-ignore-case on' >> ~/.inputrc
 #linux
 yes | sudo apt-get install gcc make libncurses5-dev openssl libssl-dev
@@ -21,3 +23,6 @@ yes | sudo apt-get install libc6-dev
 yes | sudo apt-get install bison
 yes | sudo apt-get install flex
 yes | sudo apt-get install libelf-dev
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+source ~/.bashrc
--- vimrc
+++ vimrc
@@ -16,12 +16,12 @@ Plugin 'VundleVim/Vundle.vim'
 " The following are examples of different formats supported.
 " Keep Plugin commands between vundle#begin/end.
 " plugin on GitHub repo
-Plugin 'tpope/vim-fugitive'
+" Plugin 'tpope/vim-fugitive'
 " plugin from http://vim-scripts.org/vim/scripts.html
 " Git plugin not hosted on GitHub
-Plugin 'git://git.wincent.com/command-t.git'
+" Plugin 'git://git.wincent.com/command-t.git'
 " git repos on your local machine (i.e. when working on your own plugin)
-Plugin 'file:///home/gmarik/path/to/plugin'
+" Plugin 'file:///home/gmarik/path/to/plugin'
 " The sparkup vim script is in a subdirectory of this repo called vim.
 " Pass the path to set the runtimepath properly.
 Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}

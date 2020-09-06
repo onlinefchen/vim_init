713a82b9b5157da87f643e1741db4d9a1318f744
Chen Feng <puck.chen@foxmail.com>
2020-09-06 11:47:19 +0800

    proxy add proxy
    Signed-off-by: Chen Feng <puck.chen@foxmail.com>

--- proxy.sh
+++ proxy.sh
@@ -1,7 +1,7 @@
 #vim
 yes | sudo apt-get install iptables ipset dnsmasq shadowsocks shadowsocks-libev
 mkdir ~/work/github
-cd ~/work
+cd ~/work/github
 git clone https://github.com/zfl9/ss-tproxy
 cd ss-tproxy
 sudo install ss-tproxy /usr/local/bin
@@ -13,9 +13,13 @@ cd -
 cd vim_init
 sudo cp ss.json /etc
 sudo cp ss-tproxy.conf /etc/ss-tproxy/ss-tproxy.conf
+cd -
 
 git clone https://github.com/zfl9/chinadns-ng
 cd chinadns-ng
 make && sudo make install
+cd -
+
+echo "alias proxy='kill -9 (pidof ss-redir);sudo systemctl stop systemd-resolved; sudo ss-tproxy start'" ~/.bashrc
 
 cd -
